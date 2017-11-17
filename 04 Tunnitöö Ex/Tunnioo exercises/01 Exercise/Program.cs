b7e1d46259a851d14b1ce103b56b4e8638c62a0f
SilverBarnabas <33309312+SilverBarnabas@users.noreply.github.com>
2017-11-17 13:54:00 +0200

    Asks about your day

---
+++ 04 Tunnitöö Ex/Tunnioo exercises/01 Exercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            Console.Write("Kuidas Teil läheb? ");
+            Console.Write("");
+            string input = Console.ReadLine();
+
+            Console.Write($"\nMiks Teil läheb {input} ? ");
+            string input2 = Console.ReadLine();
+
+            int number = rnd.Next(2);
+
+            switch (number)
+            {
+                case 0:
+                    {
+                        Console.WriteLine("Huvitav");
+                        break;
+                    }
+                case 1:
+                    {
+                        Console.WriteLine("Tore!");
+                        break;
+                    }
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
