7098bbdb7671eb6abee03a34950bd3494c3aadf7
Grete Liblikman <32923450+GreteLiblikman@users.noreply.github.com>
2018-02-12 21:11:42 +0200

    Add files via upload

---
+++ Hangmen/Hangmen/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Random word chooser
+            string[] Words = new string[4] { "TAHELAEV", "KUUUURIA", "MARI", "TORU" };
+            Random rnd = new Random();
+            string Word = Words[rnd.Next(Words.Length)].ToUpper();
+            Console.WriteLine($"{Word}");
+
+            // Tell the user the number of letters with dashes
+            char[] WordLenght = new char[Word.Length];
+            for (int i = 0; i < WordLenght.Length; i++)
+            {
+                WordLenght[i] = '_';
+            }
+
+            for (int i = 0; i < WordLenght.Length; i++)
+            {
+                Console.Write(WordLenght[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            int count = 0;
+            int Lives = 0;
+            
+            List<string> Letters = new List<string>(); //kasutatud tahtede jaoks
+            
+            do
+            {
+                Console.WriteLine($"\nGuessed letters {string.Join(",", Letters)}");
+                Console.Write("\nSisesta taht: ");
+                char input = Console.ReadLine().ToUpper().ToCharArray()[0];
+                Console.WriteLine();
+                
+                //muudab chari stringiks  ning lisab sisestatud  t2he Letters listi                              
+                string array = input.ToString().ToUpper();
+                Letters.Add(array);
+
+
+                for (int i = 0; i < Word.Length; i++)
+                {
+                    //asendab dashi tahega
+                    if (Word[i] == input)
+                    {
+                        count++; //kontrollib counti millal v'ljuda
+                        WordLenght[i] = input; //asendab dashi inputiga
+                        Console.Write(WordLenght[i] + " ");
+                    }
+                    //kirjutab valja mis on juba arvatud                    
+
+                    else
+                    {
+                        Lives++;                      
+                        Console.Write(WordLenght[i] + " ");
+                        
+                    }
+                    
+                }
+                
+            }
+
+            while (count < WordLenght.Length || Lives < 5);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\n\nYou guessed right!!");
+            Console.ResetColor();
+            Console.ReadLine();
+   
+        }
+    }
+}
